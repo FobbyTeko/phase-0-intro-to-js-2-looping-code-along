cc9cd1106ea3e924f395ad4d9f9228bc329a8aae
Tiri Tuatai <tiri.jnr@gmail.com>
2024-02-28 22:05:09 +0800

    Looping Done again

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function writeCards(name, other) {
     return cards 
 }
 
+const value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 function countDown (value) {
     for(let i = value; i > -1; i--) {
         console.log(i)
