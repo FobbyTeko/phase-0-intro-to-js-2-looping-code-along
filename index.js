f4b6f217c8c62d5d752f4e1138c3769e3980fcac
Tiri Tuatai <tiri.jnr@gmail.com>
2024-02-28 22:02:28 +0800

    Looping Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name, other) {
+    let cards = []
+    for(let i in name) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${other} gift!`)
+    }
+    return cards 
+}
+
+function countDown (value) {
+    for(let i = value; i > -1; i--) {
+        console.log(i)
+    }
+}
